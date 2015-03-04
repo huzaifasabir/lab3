f6e4f3331729cc5ca04ee51fc59f4754ed52b4fe
huzaifasabir <huzaifa249@gmail.com>
2015-03-04 21:14:52 +0500

    Create serverTest.java

---
+++ serverTest.java
@@ -0,0 +1,21 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class serverTest {
+
+	@Test
+	public void test() throws IOException {
+		javaServer s = new javaServer();
+		s.server();
+		
+		
+		
+		
+	}
+
+}
